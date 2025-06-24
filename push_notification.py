913e412cef049ad7d4b3cdbd93766a5bd77d6ee5
Doubletree <108912608+doubletree6@users.noreply.github.com>
2025-06-24 18:19:19 +0800

    Update push_notification.py to send check-in results

--- push_notification.py
+++ push_notification.py
@@ -6,27 +6,19 @@ from os import environ
 from json import dumps
 from requests import post
 
-def dishes():
+import sys
+
+def send_notification(message):
     token = environ.get('token')
     if not token:
         # If the token is not present, stop sending push notification
         return 'PushPlus: No token configured, cannot send push notification.'
     url = 'http://www.pushplus.plus/send/'
-    # List of dishes
-    dishes = ["自助", "茶香鸡米饭", "羊汤", "泡馍", "拉面", "水饺", "BONUS TIME！", "炒菜", "鸡公煲", "板面", "冒菜", "馄饨", "炸串", "牛杂面", "炒饭", "烤鸭饭"]
-    afternoon_dishes = ["沙拉", "炸串", "牛杂面"]
-
-    current_hour = datetime.datetime.now().hour
-    if current_hour < 6: # Assuming afternoon dishes are excluded before noon
-        dishes = [dish for dish in dishes if dish not in afternoon_dishes]
-
-    # Randomly choose a dish from list
-    chosen_dish = random.choice(dishes)
 
     data = {
         "token": token,
-        "title": "这顿吃 " + chosen_dish,
-        "content": "这顿吃 " + chosen_dish,
+        "title": "NatPierce 签到结果",
+        "content": message,
     }
 
     headers = {'Content-Type': 'application/json'}
@@ -34,4 +26,9 @@ def dishes():
     return rsp.text
 
 if __name__ == '__main__':
-    print(dishes())
+    if len(sys.argv) > 1:
+        checkin_message = sys.argv[1]
+        print(send_notification(checkin_message))
+    else:
+        print("Usage: python push_notification.py <checkin_message>")
+        sys.exit(1)
